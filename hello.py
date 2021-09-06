1ea99f76b241a6f2640ed444cda251c516a4ccb2
AmarNagim <99069493@mydavinci.nl>
2021-09-06 10:20:32 +0200

    second_commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+ print("""Hello world van Amar:                                   
+         pppp    y     y   ttttttt   h   h    oooo   nnnn          
+         p   p    y   y       t      h   h   o    o  n   n
+         p   p     y y        t      h   h   o    o  n   n
+         pppp       y         t      hhhhh   o    o  n   n
+         p          y         t      h   h   o    o  n   n
+         p          y         t      h   h   o    o  n   n
+         p          y         t      h   h    oooo   n   n""")  # Naam: Amar Nagim Studentennummer: 99069493   
+      
